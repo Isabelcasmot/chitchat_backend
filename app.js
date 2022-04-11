b3c5091208e3af5945a9d8c0dac4efe35da04c78
Isabelcasmot <isabelcasmot@gmail.com>
2022-04-11 16:32:27 +0100

    The Form send the users data to mysql

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // Fichero que contiene todos los manejadores de la aplicación
 const express = require('express');
-
+const cors = require('cors');
 // Crear la APP
 const app = express();
 
 // TODO: Configuración de los manejadores
-
+app.use(cors())
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
