a9246e604cbd008e742c4e77c94c216c26c09be8
iaraszuir <iaraszuir@gmail.com>
2022-04-20 16:19:38 +0200

    minichange

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { checkToken } = require('../../helpers/middlewares')
 
-router.use('/users', checkToken, require('./users.route'));
+router.use('/users', require('./users.route'));
 router.use('/events', checkToken, require('./events.route'))
 
 module.exports = router;
